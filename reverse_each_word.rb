91c7f58b6471be98377c516e8c593b42c366cafa
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-15 23:54:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
   sentence_split_array = sentence.split
   reversed_word_array = []
   sentence_split_array.each do |word|
-    reversed_word_array << #{word.reverse}
+    reversed_word_array << word.reverse
 end
   reversed_word_array.join
 end
