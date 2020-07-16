e1f5611707c468ba806939d65c89f35f42a32eac
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-16 00:01:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 
 
 def reverse_each_word(sentence)
-  sentence_split_array = sentence.split(" ")
+  sentence_split_array = sentence.split
   reversed_word_array = []
   sentence_split_array.each do |word|
     reversed_word_array << word.reverse
 end
-  reversed_word_array.join
+  reversed_word_array.join(" ")
 end
 
 # make string into an array
